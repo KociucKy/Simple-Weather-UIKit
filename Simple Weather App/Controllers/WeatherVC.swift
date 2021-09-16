e6936abb0391cd22e4ab92dbb32f7977ed3453f5
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-09-16 09:58:01 +0200

    Basic set-up of the UITableView inside WeatherVC

--- Simple Weather App/Controllers/WeatherVC.swift
+++ Simple Weather App/Controllers/WeatherVC.swift
@@ -1,9 +1,35 @@
 import UIKit
 
 class WeatherVC: UIViewController {
-
+    @IBOutlet weak var forecastTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        forecastTableView.dataSource = self
+        forecastTableView.delegate = self
     }
 }
 
+//MARK: - UITableView DataSource Methods
+extension WeatherVC: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = UIColor.white
+        return cell
+    }
+}
+
+//MARK: - UITableView Delegate Methods
+extension WeatherVC: UITableViewDelegate{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 7
+    }
+    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return CGFloat(150)
+//    }
+}
