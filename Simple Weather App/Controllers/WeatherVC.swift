60399fad74f3f401a91ac26cbe9250f1e19140c2
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-09-16 11:53:59 +0200

    I added all needed Outlets and Action Methods. Apart from that I created two methods that create shadows, one below image and one below button

--- Simple Weather App/Controllers/WeatherVC.swift
+++ Simple Weather App/Controllers/WeatherVC.swift
@@ -1,24 +1,45 @@
 import UIKit
 
 class WeatherVC: UIViewController {
+    //MARK: - Outlets
+    @IBOutlet weak var localizationTextField: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var airPollutionButton: UIButton!
     @IBOutlet weak var forecastCollectionView: UICollectionView!
     
+    //MARK: - VC Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-//        forecastCollectionView.dataSource = self
+        imageShadow(weatherImage)
+        buttonShadow(airPollutionButton)
+    }
+    
+    //MARK: - Actions
+    @IBAction func airPollutionStatusPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func localizeMePressed(_ sender: Any) {
+    }
+    
+    //MARK: - Methods
+    func imageShadow(_ image: UIImageView){
+        image.layer.shadowColor = UIColor(named: "ShadowColor")?.cgColor
+        image.layer.shadowOpacity = 1
+        image.layer.shadowOffset = CGSize.init(width: 2, height: 2)
+        image.layer.shadowRadius = 5
+    }
+    
+    func buttonShadow(_ button: UIButton){
+        button.layer.shadowColor = UIColor(named: "ShadowColor")?.cgColor
+        button.layer.shadowOpacity = 1
+        button.layer.shadowOffset = CGSize.init(width: 2, height: 2)
+        button.layer.shadowRadius = 5
     }
 }
 
-//MARK: - UICollectionView DataSource Methods
-//extension WeatherVC: UICollectionViewDataSource{
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 7
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = UICollectionViewCell()
-//        cell.backgroundColor = UIColor.white
-//        return cell
-//    }
-//}
 
